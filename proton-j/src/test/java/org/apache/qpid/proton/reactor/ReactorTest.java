0dbb1fa97cf9075b72b9cbecc4bbb2b458efa2a1
Bozo Dragojevic <bozzo@digiverse.si>
2015-07-01 13:16:52 +0200

    Check that reactor is accessible through all tested event types

--- proton-j/src/test/java/org/apache/qpid/proton/reactor/ReactorTest.java
+++ proton-j/src/test/java/org/apache/qpid/proton/reactor/ReactorTest.java
@@ -121,6 +121,7 @@ public class ReactorTest {
 
         @Override
         public void onUnhandled(Event event) {
+            assertNotNull(event.getReactor());
             actual.add(event.getType());
         }
 
